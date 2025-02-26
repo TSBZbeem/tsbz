9782273fce71c858fea251aa84521d7b41578918
TSBZbeem <beemza2288@gmail.com>
2025-02-26 22:21:53 +0700

    world create

--- 2dGame/src/entity/Player.java
+++ 2dGame/src/entity/Player.java
@@ -13,18 +13,24 @@ public class Player extends Entity{
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH){
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - (gp.tileSize/2);
+        screenY = gp.screenHeight/2 - (gp.tileSize/2);
 
         setDefaultValues();
         getPlayerImage();
     }
     public void setDefaultValues(){
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
-        direction ="up";
+        direction ="down";
     }
     public void getPlayerImage(){
         try{
@@ -44,16 +50,16 @@ public class Player extends Entity{
         if(keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true){
             if(keyH.upPressed == true){
                 direction = "up";
-                y -= speed;
+                worldY -= speed;
             }else if(keyH.downPressed == true){
                 direction = "down";
-                y += speed;
+                worldY += speed;
             }else if(keyH.leftPressed == true){
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             }else if(keyH.rightPressed == true){
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
     
             spriteCounter++;
@@ -72,7 +78,7 @@ public class Player extends Entity{
     public void draw(Graphics2D g2){
         // g2.setColor(Color.white);
 
-        // g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        // g2.fillRect(worldX, worldY, gp.tileSize, gp.tileSize);
         BufferedImage image = null;
 
         switch(direction){
@@ -109,6 +115,6 @@ public class Player extends Entity{
             }
                 break;
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
